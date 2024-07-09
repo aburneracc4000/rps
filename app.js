891090be1e116896588176359262698d441e5fe7
theburneracc4000 <aburneracc4000@gmail.com>
2024-07-08 22:00:22 -0400

    add function getComputerChoice

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("hi :)")
+  console.log("hi :)");
+
+  
+
+   function getComputerChoice() {
+    let rps = Math.floor(Math.random() * 100) +1;
+
+       if (rps >= 0 && rps <= 33) {
+
+         return "Rock";
+
+       }else if (rps >= 34 && rps <= 66) {
+          
+         return "paper";
+
+       }else if (rps >= 67 && rps <=100) {
+
+        return "scissors";
+
+       }
+}
