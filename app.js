f26543463d635b2fc6f03a3fc2ba197eefc02200
theburneracc4000 <aburneracc4000@gmail.com>
2024-07-08 22:22:34 -0400

    edit funcion getComputerChoice, add pseudocode to getComputerChoice

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
   console.log("hi :)");
 
   
-
+  // SET rps to 1 out of 100
+  //   IF rps is 1 to 33 THEN
+  //      DISPLAY Rock
+  //   ELSE IF rps is 34 to 66 THEN
+  //      DISPLAY Paper
+  //   ELSE IF rps is 67 to 100 THEN 
+  //   DISPLAY Scissors
+  //   END IF
+  //   END ELSE IF
+  //   END ELSE IF
    function getComputerChoice() {
     let rps = Math.floor(Math.random() * 100) +1;
 
