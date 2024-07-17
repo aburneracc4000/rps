008c316e8d4f7f930153a3d65de27c32655b87a1
theburneracc4000 <aburneracc4000@gmail.com>
2024-07-17 05:57:58 -0400

    add playGame function, edit playRound

--- app.js
+++ app.js
@@ -2,10 +2,6 @@
 
   let counter = 0;
   
-  let userScore = 0;
-
-  let compScore = 0;
-  
   // SET rps to 1 out of 100
   //   IF rps is 1 to 33 THEN
   //      DISPLAY Rock
@@ -70,6 +66,16 @@
     }
    }
 
+   //GET playRound value
+   //  REPEAT playRound function UNTIL
+   //    userScore or compScore is equal to 3
+   //  DISPLAY the winner of the game 
+   function playGame() {
+
+      let userScore = 0;
+
+      let compScore = 0;
+
    // GET humanChoice & compChoice values
    // DETERMINE the winner from humanChoice & compChoice
    //   IF humanChoice wins THEN
@@ -85,56 +91,85 @@
    //   END IF
    function playRound(humanChoice, compChoice) {
 
-    if (humanChoice === "rock" && compChoice === "paper") { 
-
-      ++compScore;
-   
-      return ("they have chosen Paper, you lose!");
-   
-   }else if (humanChoice === "rock" && compChoice === "scissors") {
-
-      ++userScore;
-
-      return("they have chosen Scissors, you win!");
-
-   }else if (humanChoice === "paper" && compChoice === "scissors") {
-
-      ++compScore;
-
-      return("they chose Scissors, you lose!");
-
-   }else if (humanChoice === "paper" && compChoice === "rock") {
-
-      ++userScore;
-
-      return("they chose Rock, you win!");
-
-   }else if (humanChoice === "scissors" && compChoice === "rock") {
-
-      ++compScore;
-
-      return("they chose Rock, you lose!");
-
-   }else if (humanChoice === "scissors" && compChoice === "paper") {
-
-      ++userScore;
-
-      return("they chose Paper, you win!");
-
-   }else if (humanChoice === compChoice) {
-
-      return("its a tie!");
-
-   }else if (humanChoice === "invalid") {
-
-      return("thats not an answer!");
-
-   } 
-   }
-   const userSelection = getUserInput();
-
-   const compSelection = getComputerChoice();
-
-   playRound(userSelection, compSelection);
-
+      if (humanChoice === "rock" && compChoice === "paper") { 
   
+        ++compScore;
+     
+        console.log("they have chosen Paper, you lose!");
+     
+     }else if (humanChoice === "rock" && compChoice === "scissors") {
+  
+        ++userScore;
+  
+        console.log("they have chosen Scissors, you win!");
+  
+     }else if (humanChoice === "paper" && compChoice === "scissors") {
+  
+        ++compScore;
+  
+        console.log("they chose Scissors, you lose!");
+  
+     }else if (humanChoice === "paper" && compChoice === "rock") {
+  
+        ++userScore;
+  
+        console.log("they chose Rock, you win!");
+  
+     }else if (humanChoice === "scissors" && compChoice === "rock") {
+  
+        ++compScore;
+  
+        console.log("they chose Rock, you lose!");
+  
+     }else if (humanChoice === "scissors" && compChoice === "paper") {
+  
+        ++userScore;
+  
+        console.log("they chose Paper, you win!");
+  
+     }else if (humanChoice === compChoice) {
+  
+        console.log("its a tie!");
+  
+     }else if (humanChoice === "invalid") {
+  
+        console.log("thats not an answer!");
+  
+     }
+
+     
+   
+   }
+
+   do {
+
+      playRound(getUserInput(), getComputerChoice());
+
+    } while (userScore < 3 && compScore < 3)
+   
+     //const userSelection = getUserInput();
+  
+     //const compSelection = getComputerChoice();
+
+     //return(playRound(userSelection, compSelection));
+
+   if (userScore >= 3) {
+
+      console.log("You have won!")
+
+      return
+
+   }else if (compScore >= 3) {
+
+      console.log("You have lost!")
+
+      return
+
+   }
+ 
+   
+   }
+
+
+   console.log(playGame());
+
